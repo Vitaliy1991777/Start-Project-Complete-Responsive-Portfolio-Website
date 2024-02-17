f9eeebb2ad4dd55a3d3419ef25dddf69b142714c
Vitaly <86892288+Vitaliy1991777@users.noreply.github.com>
2024-02-17 19:21:00 +0500

    Portfolio

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('avtive');
+    navbar.classList.toggle('active');    
 }
 
 // scroll sections
@@ -24,6 +24,13 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active section for animation on scroll
+            sec.classList.add('show-animate');
+
+        }
+        // if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
         }
     });
     // sticky header
@@ -31,10 +38,13 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
-
     // remove toggle icon and navbar when click navbar links (scroll)
-
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');  
 
     // animation footer on scroll
+    let footer = document.querySelector('footer');
 
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.
+    scrollHeight);
 }
